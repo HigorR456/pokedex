94d3a6af7cbf99615375710d316cb01b6c4b4481
HigorR456 <higor.ruan456@gmail.com>
2023-01-13 11:38:00 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Pokemon />
+    </div>
+  )
+}
+
+const Pokemon = () => {
+  const [info, setInfo] = useState([]);
+  const [num, setNum] = useState(1);
+  const [listNum, setListNum] = useState(20);
+
+
+  const handleListNum = () => {
+    setListNum(listNum+10);
+    setNum(num+1);
+  }
+
+
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.name);
+      setInfo(data.name);
+      
+      if (num<listNum) {
+        setNum(num+1);
+      } else {
+        console.log('ok')
+      }
+
+      document.querySelector('.listWrapper')?.insertAdjacentHTML('beforeend', 
+      `<div class='divWrapper'>
+      <span><img class='image' src=${data.sprites.front_default}></img></span>
+      <p class='name'>${data.name}</p>
+      <p>&nbsp;- EXP&nbsp;</p>
+      <p class='experience'>${data.base_experience}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      `);
+
+    })}, [num])
+
+  return (
+    <div className='listWrapper'>
+      <button onClick={handleListNum} >Show +10</button>
+      
     </div>
   )
 }
